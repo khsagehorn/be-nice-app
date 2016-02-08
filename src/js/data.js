cf3af579168c1f2184b91999fb81f587e6c29c6a
Daniel Klein <danhirschklein@gmail.com>
2016-02-08 14:00:24 -0700

    First four compliments

--- src/js/data.js
+++ src/js/data.js
@@ -3,3 +3,8 @@ function NiceThing(id, text, rating){
   this.text = text;
   this.rating = rating;
 }
+
+var nice2 = new NiceThing(2, "Your hair looks nice like that.", 0);
+var nice4 = new NiceThing(4, "Your jokes are so good they make Groucho look like Karl.", 0);
+var nice6 = new NiceThing(6, "Wow! Those pants and your butt are good friends.")
+var nice8 = new NiceThing(8, "")
