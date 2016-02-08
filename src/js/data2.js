d598453a62757464f069e3fa679f2c98f46e6e8e
khsagehorn <huntersagehorn@gmail.com>
2016-02-08 14:12:23 -0700

    add data2

---
+++ src/js/data2.js
@@ -0,0 +1,11 @@
+var nice2 = new NiceThing(2, "You're doing great, buddy. Keep it up.");
+
+var nice4 = new NiceThing(4, "Your friends are totally jealous of you.");
+
+var nice6 = new NiceThing(6, "Killing it! You are absolutely killing it.");
+
+var nice8 = new NiceThing(8, "How can you have a bad day when you look that good?");
+
+var nice10 = new NiceThing(10, "I totally couldn't blame anyone in the room for checking you out right now. ON FLEEK.");
+
+var nice12 = new NiceThing(12, "Dang, kid. You good. You real good.");
