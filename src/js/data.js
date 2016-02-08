415148cdc3a9ec29af680f7561771362855df949
khsagehorn <huntersagehorn@gmail.com>
2016-02-08 14:50:43 -0700

    combine data

--- src/js/data.js
+++ src/js/data.js
@@ -4,14 +4,25 @@ function NiceThing(id, text, rating){
   this.rating = rating;
 }
 
-var nice2 = new NiceThing(2, "Your hair looks nice like that.", 0);
-var nice4 = new NiceThing(4, "Your jokes are so good they make Groucho look like Karl.", 0);
-var nice6 = new NiceThing(6, "Wow! Those pants and your butt are good friends.", 0);
-var nice8 = new NiceThing(8, "You should sing more.", 0);
-var nice10 = new NiceThing(10, "Nobody likes the people who don't like you.", 0);
-var nice12 = new NiceThing(12, "You are made from the same particles as the sun.", 0);
-var nice14 = new NiceThing(14, "All of history led to you.", 0)
-var nice16 = new NiceThing(16, "Nobody likes the people who don't like you.", 0);
-var nice18 = new NiceThing(18, "Your opinions are correct.", 0);
 
-var nice18 = new NiceThing(20, "In the future they will tell your tale.", 0);
+
+var nice1 = new NiceThing(1, "You're doing great, buddy. Keep it up.", 0);
+var nice2 = new NiceThing(2, "Your hair looks nice like that.", 0);
+var nice3 = new NiceThing(3, "Your friends are totally jealous of you.", 0);
+var nice4 = new NiceThing(4, "Your jokes are so good they make Groucho look like Karl.", 0);
+var nice5 = new NiceThing(5, "Killing it! You are absolutely killing it.", 0);
+var nice6 = new NiceThing(6, "Wow! Those pants and your butt are good friends.", 0);
+var nice7 = new NiceThing(7, "How can you have a bad day when you look that good?", 0);
+var nice8 = new NiceThing(8, "You should sing more.", 0);
+var nice9 = new NiceThing(9, "I totally couldn't blame anyone in the room for checking you out right now. ON FLEEK.", 0);
+var nice10 = new NiceThing(10, "Nobody likes the people who don't like you.", 0);
+var nice11 = new NiceThing(11, "Dang, kid. You good. You real good.");
+var nice12 = new NiceThing(12, "You are made from the same particles as the sun.", 0);
+var nice13 = new NiceThing(13, "You deserve pizza.", 0);
+var nice14 = new NiceThing(14, "All of history led to you.", 0)
+var nice15 = new NiceThing(15, "High five!", 0);
+var nice16 = new NiceThing(16, "Nobody likes the people who don't like you.", 0);
+var nice17 = new NiceThing(17, "You are strong! You are beautiful! You are desireable to others!", 0);
+var nice18 = new NiceThing(18, "Your opinions are correct.", 0);
+var nice19 = new NiceThing(19, "Winning!",0);
+var nice20 = new NiceThing(20, "In the future they will tell your tale.", 0);
