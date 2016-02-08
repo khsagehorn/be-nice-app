46339d6b680add50decefc261e410bdd6b9c7e2b
Daniel Klein <danhirschklein@gmail.com>
2016-02-08 14:03:59 -0700

    More compliments

--- src/js/data.js
+++ src/js/data.js
@@ -6,5 +6,9 @@ function NiceThing(id, text, rating){
 
 var nice2 = new NiceThing(2, "Your hair looks nice like that.", 0);
 var nice4 = new NiceThing(4, "Your jokes are so good they make Groucho look like Karl.", 0);
-var nice6 = new NiceThing(6, "Wow! Those pants and your butt are good friends.")
-var nice8 = new NiceThing(8, "")
+var nice6 = new NiceThing(6, "Wow! Those pants and your butt are good friends.", 0);
+var nice8 = new NiceThing(8, "You should sing more.", 0);
+var nice10 = new NiceThing(10, "Nobody likes the people who don't like you.", 0);
+var nice12 = new NiceThing(12, "You are made from the same particles as the sun.", 0);
+var nice14 = new NiceThing(14, "All of history led to you.", 0)
+var nice16 = new NiceThing(16, "Nobody likes the people who don't like you.", 0);
